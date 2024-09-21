11c59a7c71a526092fc85366bc6c29fa42346c9b
Nelson Carvalho <nelsoncarvalho@10.0.0.101>
2024-09-21 15:57:45 -0400

    Manaus dataset

---
+++ R/manaus.R
@@ -0,0 +1,15 @@
+#' Manaus Minimum Monthly relative air humidity
+#'
+#' A time series dataset consisting of the minimum relative humidity observed in the city of Manaus from January 2009 to December 2020. This dataset is a sample of size n = 144 monthly blocks.
+#'
+#' @format ## `manaus' A tibble with 144 rows and 5 columns
+#' \describe{
+#'   \item{year}{The year in which this data was observed}
+#'   \item{rh}{Relative Humidity}
+#'   \item{dbt}{Dry Bulb Temperature}
+#'   \item{wbt}{Wet Bulb Temperature}
+#'   \item{month}{The month in which this data was observed}
+#' }
+#' 
+#' @source Source information to be provided later
+"manaus"
