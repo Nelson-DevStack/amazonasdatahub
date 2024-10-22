e1acc175a32907d5a80b46c0a42a16fd83980886
Nelson Carvalho <nelsoncarvalho@10.0.0.104>
2024-10-21 23:00:44 -0400

    added fixed rionegro_amazonas

--- R/rionegro_amazonas.R
+++ R/rionegro_amazonas.R
@@ -1,16 +1,15 @@
-#' rionegro_amazonas - A dataset of the Rio Negro River (Amazonas) level from 2002 to 2023
+#' rionegro_amazonas - A dataset of the Rio Negro River (Amazonas) level from 2003 to 2023
 #'
 #' This dataset contains data of the Rio Negro River level in the state of Amazonas (Brazil), compiling information on years, semesters and days, along with the river's level in meters and whether it filled or dropped (in centimeters).
 #'
 #' @format ## `rionegro_amazonas' A data frame with 8003 rows and 7 columns:
 #' \describe{
-#'   \item{date}{Measurement full date}
-#'   \item{year}{Measurement year}
-#'   \item{month}{Measurement month}
+#'   \item{level_m}{River level in meters}
+#'   \item{increase_decrease_cm}{Wheter the river level filled or dropped (in centimeters)}
 #'   \item{day}{Measurement day}
+#'   \item{month}{Measurement month}
+#'   \item{year}{Measurement year}
 #'   \item{semester}{Measurement semester}
-#'   \item{level_meters}{River level in meters}
-#'   \item{filled_dropped_cm}{Wheter the river level filled or dropped (in centimeters)}
 #' }
 #' 
 #' @source https://portodemanaus.com.br/nivel-do-rio-negro/
