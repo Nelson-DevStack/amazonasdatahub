703f8b7aac21763c82d50c39218ebe205246da78
Nelson Carvalho <nelsoncarvalho@10.0.0.101>
2025-02-23 15:19:38 -0400

    aids_amazonas dataset

---
+++ R/aids_amazonas.R
@@ -0,0 +1,16 @@
+#' aids_amazonas - A dataset of AIDS occurrences from 2011 to 2023 in Amazonas
+#'
+#' This dataset contains data of the AIDS occurrences in each municipality from Amazonas.
+#'
+#' @format ## `aids_amazonas' A data frame with 1612 rows and 5 columns:
+#' \describe{
+#'   \item{code_muni}{Municipality code}
+#'   \item{name_muni}{Municipality name}
+#'   \item{cases}{Case count}
+#'   \item{gender}{Gender of the infected individual - M for Male and F for female}
+#'   \item{year}{Year of observation}
+#' }
+#' 
+#' @source https://indicadores.aids.gov.br/
+#' 
+"aids_amazonas"
