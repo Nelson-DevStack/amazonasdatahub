e7f869fc2e48ab374899979dfa2ce61f238a1ed8
Nelson-DevStack <n.devpro@protonmail.com>
2025-08-23 15:17:19 -0400

    renaming school_read_levels to slr_muni

--- R/srl_muni.R
+++ R/srl_muni.R
@@ -1,8 +1,8 @@
-#' school_read_levels - Physical literacy and reading performance of amazonian schoolchildren
+#' srl_muni - School Read Levels - Physical literacy and reading performance of amazonian schoolchildren
 #'
 #' This is a dataset from an association, cross-sectional, quantitative study carried out with Amazonian schoolchildren (n=100; aged between 10 and 13 years) from 3 public schools in the municipality of Borba (Amazonas).
 #'
-#' @format ## `school_read_levels' A data frame with 100 rows and 17 columns:
+#' @format ## `srl_muni' A data frame with 100 rows and 17 columns:
 #' \describe{
 #'   \item{school}{School of observation}
 #'   \item{sex}{Sex - 1 for Masculine and 2 for Feminine}
@@ -17,12 +17,12 @@
 #'   \item{pmc}{Perceived Motor Competence (PMC) assesses the child's self-perception regarding their ability to perform motor movements. Measured by a psychometric scale}
 #'   \item{kupa}{Knowledge and Understanding of Physical Activity (KUPA) - Evaluates knowledge regarding the benefits, safety, and engagement in physical activity. Measured by scores obtained in objective questions}
 #'   \item{sb}{Sedentary Behavior - Evalutes the exposition time to sedentary behaviors. Measured by psychometric scale}
-#'   \item{pa}{Pyshical Activity (PA) - Level of physical activity in children, based on the frequency and duration of routine daily activities. Measured by score on the physical activity frequency questions}
-#'   \item{sts_s}{Supine To Stand (in seconds) - Measures the time (in seconds) it takes for the child to stand up from the floor, from a supine position to a standing position}
+#'   \item{pa}{Pyshical Activity (PA) - Level of physical activity ian children, based on the frequency and duration of routine daily activities. Measured by score on the physical activity frequency questions}
+#'   \item{mc}{Motor Competence - Supine To Stand (in seconds) - Measures the time (in seconds) it takes for the child to stand up from the floor, from a supine position to a standing position}
 #'   \item{tde}{Total Score on the TDE test - Measures reading level based on time and accuracy in word reading}
 #'   \item{tde_categorical}{Categorical description of the TDE Score}
 #' }
 #'
 #' @source https://tede.ufam.edu.br/handle/tede/10627
 #'
-"school_read_levels"
+"slr_muni"
